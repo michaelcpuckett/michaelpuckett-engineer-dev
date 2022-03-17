4019fdd12b7dae5fbbde4ceb2404451d7d557401
Michael Puckett <michaelcpuckett@gmail.com>
2022-03-16 20:24:33 -0400

    working function

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,74 @@
 const functions = require('firebase-functions');
+const firebaseAdmin = require('firebase-admin');
 const crypto = require('crypto');
 const express = require('express');
-const app = express();
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const privkey = require('./private-key.js').key;
 
-app.use(express.json({strict: false}));
-
-({
-    '@context': 'https://www.w3.org/ns/activitystreams',
-    'id': 'https://mastodon.social/users/mpuckett#follows/6123457/undo',
-    'type': 'Undo',
-    'actor': 'https://mastodon.social/users/mpuckett',
-    'object': {
-        'id': 'https://mastodon.social/9424d702-1db3-42f0-8f11-bde5bf2cba2c',
-        'type': 'Follow',
-        'actor': 'https://mastodon.social/users/mpuckett',
-        'object': 'https://michaelpuckett.engineer/as/actor'
-    }
+firebaseAdmin.initializeApp({
+    apiKey: "AIzaSyB6ocubyR0Ddg7NdmA1bIFiuOH4nnVSI4w",
+    projectId: "pickpuck-com",
+    databaseURL: "https://pickpuck-com.firebaseio.com",
 });
+
+const app = express();
+app.use(express.json({strict: false}));
 
 const localActor = 'https://michaelpuckett.engineer/as/actor';
 
-app.post('*', (req, res) => {
+const signAndSendToActorInbox = async (message) => {
+    firebaseAdmin.database().ref(`/as/outbox/${Date.now()}`).set(message);
+    const foreignActor = message.object.actor;
+    const foreignActorInbox = `${foreignActor}/inbox`;
+    const foreignDomain = new URL(foreignActorInbox).hostname;
+    const foreignPathName = new URL(foreignActorInbox).pathname;
+
+    // sign
+    const digestHash = crypto.createHash('sha256').update(JSON.stringify(message)).digest('base64');
+    const signer = crypto.createSign('sha256');
+    const dateString = new Date().toUTCString();
+    const stringToSign = `(request-target): post ${foreignPathName}\nhost: ${foreignDomain}\ndate: ${dateString}\ndigest: SHA-256=${digestHash}`;
+    signer.update(stringToSign);
+    signer.end();
+    const signature = signer.sign(privkey);
+    const signature_b64 = signature.toString('base64');
+    const signatureHeader = `keyId="${localActor}#main-key",headers="(request-target) host date digest",signature="${signature_b64}"`;
+    
+    // send
+    await fetch(foreignActorInbox, {
+        method: 'post',
+        body: JSON.stringify(message),
+        headers: {
+            'Content-Type': 'application/json',
+            'Host': foreignDomain,
+            'Date': dateString,
+            'Digest': `SHA-256=${digestHash}`,
+            'Signature': signatureHeader
+        }
+    }).then(async (response) => {
+        const responseBody = await response.text();
+        console.log({responseBody});
+        // record response
+        firebaseAdmin.database().ref(`/as/inbox/${Date.now()}`).set({responseBody});
+    }).catch((error) => {
+        console.log('ERROR...');
+        console.log({error});
+    });
+};
+
+const sendAcceptMessage = async (object) => {
+    const guid = crypto.randomBytes(16).toString('hex');
+    const message = {
+      '@context': 'https://www.w3.org/ns/activitystreams',
+      'id': `https://michaelpuckett.engineer/as/${guid}`,
+      'type': 'Accept',
+      'actor': localActor,
+      'object': object,
+    };
+    await signAndSendToActorInbox(message);
+};
+
+app.post('*', async (req, res) => {
     try {
         const data = JSON.parse(req.body.toString());
         const {
@@ -30,47 +78,27 @@ app.post('*', (req, res) => {
             object
         } = data;
 
+        // record request
+        firebaseAdmin.database().ref(`/as/inbox/${Date.now()}`).set(data);
+
         if (type === 'Follow' && object === localActor) {
-            console.log(`SAVE TO DB HERE ${actor} from ${req.domain}`);
-            const body = {
-                "@context": "https://www.w3.org/ns/activitystreams",
-                "id": "https://michaelpuckett.engineer/as/accept/",
-                "type": "Accept",
-                "actor": localActor,
-                "object": {
-                    id,
-                    type,
-                    actor,
-                    object,
-                }
-            };
-            const dateString = new Date().toUTCString();
-            const digestHash = crypto.createHash('sha256').update(JSON.stringify(body)).digest('base64');
-            const stringToSign = `keyId="https://michaelpuckett.engineer/as/actor#main-key",headers="(request-target) host date digest",signature="${signature_b64}"`;
-            const signer = crypto.createSign('sha256');
-            signer.update(stringToSign);
-            signer.end();
-            const privkey = ''//
-            const signature = signer.sign(privkey);
-            const signature_b64 = signature.toString('base64');
-            const header = `keyId="https://michaelpuckett.engineer/as/actor#main-key",headers="(request-target) host date digest",signature="${signature_b64}"`;
-            res.headers({
-                'Host': req.domain,
-                'Date': dateString,
-                'Digest': `SHA-256=${digestHash}`,
-                'Signature': header
+            await sendAcceptMessage(data);
+            res.status(200).send({
+                "@context": "https://www.w3.org/ns/activitystreams"
             });
-            res.method('POST');
-            res.status(200);
-            res.send(body);
         } else if (type === 'Undo') {
-            console.log('UNDO...');
-            console.log({object});
+            console.log('UNDO...(TODO)');
+            res.status(200).send({
+                "@context": "https://www.w3.org/ns/activitystreams"
+            });
+        } else {
+            console.log('OTHER...');
             res.status(200).send({
                 "@context": "https://www.w3.org/ns/activitystreams"
             });
         }
-    } catch {
+    } catch (error) {
+        console.log({error});
         console.log(req.body);
         res.status(200).send({
             "@context": "https://www.w3.org/ns/activitystreams"
@@ -79,7 +107,13 @@ app.post('*', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    console.log(req.body);
+    try {
+        const data = JSON.parse(req.body.toString());
+        // record request
+        firebaseAdmin.database().ref(`/as/inbox/${Date.now()}`).set(data);
+    } catch (error) {
+        console.log(req.body);
+    }
     res.status(200).send({
         "@context": "https://www.w3.org/ns/activitystreams"
     });
